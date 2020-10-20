dd332cbaec09d34da0ccf544edc77d400d109430
Vy Nguyen <vynguyen9699@gmail.com>
2020-10-20 23:01:41 +0300

    implement delete item in cart

--- client/src/components/CartItem/index.tsx
+++ client/src/components/CartItem/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
-import { ItemInCart } from '../../types'
+import { ItemInCart, AppState } from '../../types'
+import { userRemoveItemFromCart } from '../../redux/actions/user'
+import { getAllItemsFromLocalStorage } from '../../redux/actions/local'
+import { deleteItemFromLocalStorage } from '../../localStorage/index'
 import trashIcon from '../../icons/icons8-trash.svg'
 import './cartItem.scss'
 
@@ -9,16 +13,28 @@ type CartItemProps = {
 }
 function CartItem(props: CartItemProps) {
   const { cartItem } = props
-  console.log('cartItem', cartItem)
+  const dispatch = useDispatch()
+  const isLoggedIn = useSelector((state: AppState) => state.auth.isLoggedIn)
+  const user = useSelector((state: AppState) => state.auth.user)
   if (!cartItem.product.images) {
     return <div>Loading...</div>
+  }
+  const handleDeleteProductInCart = () => {
+    if (isLoggedIn && user) {
+      dispatch(userRemoveItemFromCart(user?._id, cartItem.product._id))
+    } else if (!isLoggedIn) {
+      deleteItemFromLocalStorage(cartItem.product._id)
+      dispatch(getAllItemsFromLocalStorage())
+    }
   }
   return (
     <div className="cart-item__wrapper">
       <img className="cart-item__img" src={cartItem.product.images[0]}></img>
       <a className="cart__item__name">{cartItem.product.name}</a>
-      <div className="cart-item__quantity">{cartItem.quantity}</div>
-      <img src={trashIcon} className="cart-item__icon" />
+      <div className="cart-item__quantity">Quantity: {cartItem.quantity}</div>
+      <button className="cart-item__button" onClick={handleDeleteProductInCart}>
+        <img src={trashIcon} />
+      </button>
       <div className="cart-item__price">{cartItem.product.price}</div>
     </div>
   )
